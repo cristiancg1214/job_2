43d827bc8dc4c67e060abe6a5b9f039428211170
Cristian Camilo Gutiérrez Vargas <cristiancamilogutierrez@reactiva.com.co>
2021-04-27 20:23:27 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,53 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-import React, { useEffect, useState } from 'react';
-import './App.css';
-
-function App(props) {
-  const { items } = props
-  let listItem = document.querySelectorAll("li")[1];
+export const App = (props) => {
+  const { items } = props;
+  const [listCopy, setListCopy] = useState([]);
+  let listItem = document.querySelectorAll("li");
+  let list = [];
   if (listItem) {
-    listItem.click();
-
-
+    // listItem.click();
   }
-  setTimeout(() => console.log(document.getElementById("root").innerHTML));
+   const onDocumentClick = (event) => {
+    let copyList = [];
+    copyList = list;
+       if (event.target.id !== "") {
+      var i = copyList.indexOf(
+        document.getElementById(event.target.id).innerHTML
+      );
+      if (i !== -1) {
+        copyList.splice(i, 1);
+        copyList.unshift(document.getElementById(event.target.id).innerHTML);
+        setListCopy([]);
+        setListCopy(copyList);
+        list = copyList;
+      }
+    }
+  };
+
+  useEffect(() => {
+    setListCopy(items);
+    list = items;
+    document.addEventListener("click", onDocumentClick);
+  }, [items]);
+  
+  
 
   return (
     <div>
       <ul>
-        {items && items.map((obj, i) => {
-          return (<li key={i}>{obj}</li>
-          )
-        })}
-
+        {listCopy &&
+          listCopy.map((obj, i) => {
+            return (
+              <li key={i} id={i}>
+                {obj}
+              </li>
+            );
+          })}
       </ul>
-
     </div>
-  )
-}
+  );
+};
 
 export default App;
